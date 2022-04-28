6d2a114afd7fb072a2656c7040721a695f6701b0
diegos9307 <diegos9307@gmail.com>
2022-04-28 18:01:05 -0500

    refactizando el codigo

--- app.js
+++ app.js
@@ -2,26 +2,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-app.use(express.json());
-
-const productos = [
-  { name: "Producto 1", price: 12.56 },
-  { name: "Producto 2", price: 10.99 },
-];
 
 mongoose.connect("mongodb://localhost:27017/products", {
   useNewUrlParser: true,
 });
 
-const ProductsSchema = new mongoose.Schema({
+const ProductSchema = new mongoose.Schema({
   name: String,
   price: Number,
 });
-const Products = mongoose.model("Products", ProductsSchema);
+const Product = mongoose.model("Product", ProductSchema);
 
 app.get("/products", async (req, res) => {
-  const showProducts = await Products.find({});
-  res.json(showProducts);
+  const products = await Product.find({});
+  res.json(products);
 });
 
 app.listen(3000, () => console.log("Running in port 3000"));
