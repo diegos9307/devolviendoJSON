6c44e7f07a3b96baa897b5ceb643283953537773
diegos9307 <diegos9307@gmail.com>
2022-04-28 17:49:38 -0500

    Implementación correcta

--- app.js
+++ app.js
@@ -7,34 +7,21 @@ app.use(express.json());
 const productos = [
   { name: "Producto 1", price: 12.56 },
   { name: "Producto 2", price: 10.99 },
-  { name: "Producto 3", price: 9.99 },
-  { name: "Producto 4", price: 14.99 },
 ];
 
-mongoose.connect(
-  process.env.MONGODB_URL || "mongodb://localhost:27017/products",
-  { useNewUrlParser: true }
-);
+mongoose.connect("mongodb://localhost:27017/products", {
+  useNewUrlParser: true,
+});
 
 const ProductsSchema = new mongoose.Schema({
-  name: { type: String },
-  price: { type: Number },
+  name: String,
+  price: Number,
 });
 const Products = mongoose.model("Products", ProductsSchema);
 
 app.get("/products", async (req, res) => {
-  productos.map((el) => {
-    const products = new Products({ name: el.name, price: el.price });
-    products.save();
-  });
-  const showProducts = await Products.find();
-  if (!showProducts) {
-    res.set("Content-Type", "application/json").json([{}]);
-  } else {
-    // res.set("Content-Type", "application/json");
-    console.log(showProducts);
-    res.set("Content-Type", "application/json").json(showProducts);
-  }
+  const showProducts = await Products.find({});
+  res.json(showProducts);
 });
 
 app.listen(3000, () => console.log("Running in port 3000"));
